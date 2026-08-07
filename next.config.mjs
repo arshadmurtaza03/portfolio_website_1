efabda91c84ead8e923992ff1a219f1c5bad4f11
Arshad Murtaza <arshadmurtaza2016@gmail.com>
2026-08-07 12:58:42 +0530

    Add Docker support and update robots/sitemap config

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
+  // Create a static production website in the "out" folder.
+  output: "export",
+
   images: {
+    // Required for static export because Nginx cannot run Next.js image optimization.
+    unoptimized: true,
+
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'raw.githubusercontent.com',
+        protocol: "https",
+        hostname: "raw.githubusercontent.com",
       },
       {
-        protocol: 'https',
-        hostname: 'github-readme-stats.vercel.app',
+        protocol: "https",
+        hostname: "github-readme-stats.vercel.app",
       },
       {
-        protocol: 'https',
-        hostname: 'github-readme-streak-stats.herokuapp.com',
+        protocol: "https",
+        hostname: "github-readme-streak-stats.herokuapp.com",
       },
     ],
   },
