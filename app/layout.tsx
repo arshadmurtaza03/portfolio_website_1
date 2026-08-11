0c79604bba9bced67b144dbdbe152da87e9f281e
Arshad Murtaza <arshadmurtaza2016@gmail.com>
2026-08-11 14:30:20 +0530

    Fix JetBrains Mono CSS variable typo

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({
 
 const jetbrainsMono = JetBrains_Mono({
   subsets: ['latin'],
-  variable: '--font-jetbrains-mono)',
+  variable: '--font-jetbrains-mono',
   display: 'swap',
 });
 
